0edeaf8df5c8d0cadb8fc45581e38b21fae96ed1
Elias Pöyliö <elias.poylio@gmail.com>
2021-01-07 11:26:26 +0200

    Readme
    Adding TODO to Readme and changing JSON structure back

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import logo from './logo.svg';
 import './App.css';
@@ -8,35 +8,54 @@ function getAll() {
     .then(data => data.json())
 }
 
-function byAmount(value) {
-  return function() {
-    console.log(value.sort((a, b) => (a.amount > b.amount) ? 1 : (a.amount === b.amount) ? ((a.amount > b.amount) ? 1 : -1) : -1 ))
-  }
-};
 
-function byName(value) {
-  return function() {
-    console.log(value.sort((a, b) => (a.name > b.name) ? 1 : (a.name === b.name) ? ((a.name > b.name) ? 1 : -1) : -1 ))
-  }
-};
 
 function App() {
-  const [allNames, setAllNames] = useState({names: []});
+  const [allNames, setAllNames] = useState({ names: [] });
+  const [namesToShow, setNamesToShow] = useState([]);
+  const [order, setOrder] = useState('byAmount');
+  var Works = () => <namesToShow onClick={displayWork} />;
+
+  console.log(namesToShow)
 
   useEffect(() => {
     let mounted = true;
     getAll()
       .then(names => {
-        if(mounted) {
-          setAllNames(names)          
+        if (mounted) {
+          setAllNames(names)
+          setNamesToShow(names.names)
         }
       })
     return () => mounted = false;
   }, [])
 
+  const displayWork = useCallback((event, { order }) => {
+    if (order === 'byName') {
+      const byName = namesToShow.sort((a, b) => (a.name > b.name) ? 1 : (a.name === b.name) ? ((a.name > b.name) ? 1 : -1) : -1)
+      setNamesToShow(byName)
+    }
+    else {
+      const byAmount = namesToShow.sort((a, b) => (a.amount > b.amount) ? 1 : (a.amount === b.amount) ? ((a.amount > b.amount) ? 1 : -1) : -1)
+      byAmount.reverse()
+      setNamesToShow(byAmount)
+    }
+  }, []);
 
-  //const byAmount = allNames.names.sort((a, b) => (a.amount > b.amount) ? 1 : (a.amount === b.amount) ? ((a.amount > b.amount) ? 1 : -1) : -1 )
-  //byAmount.reverse()
+
+
+
+
+
+  /*
+  const byAmount = allNames.names.sort((a, b) => (a.amount > b.amount) ? 1 : (a.amount === b.amount) ? ((a.amount > b.amount) ? 1 : -1) : -1)
+  byAmount.reverse()
+  setNamesToShow(byAmount)
+  
+  const byName = allNames.names.sort((a, b) => (a.name > b.name) ? 1 : (a.name === b.name) ? ((a.name > b.name) ? 1 : -1) : -1)
+  setNamesToShow(byName)
+  */
+
 
   return (
     <div className="App">
@@ -45,32 +64,24 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-      <button onClick={byAmount(allNames.names)}>By amount</button>
-      <button onClick={byName(allNames.names)}>By name</button>
+      <button onClick={() => setOrder('byAmount')}>byAmount</button>
+      <button onClick={() => setOrder('byName')}>byName</button>
       <h1>All names and amounts</h1>
-        <ul>
-        {allNames.names.map(name => <li key={name.name}>{name.name}: {name.amount}</li>)}
-        </ul>
+      <ul>
+        {namesToShow.map(name => <li key={name.name}>{name.name}: {name.amount}</li>)}
+      </ul>
     </div>
   );
 }
 
-
-/*
-      <h1>All names and amounts</h1>
+/**
+ *       <button onClick={byAmount(allNames.names)}>By amount</button>
+      <button onClick={byName(allNames.names)}>By name</button>
         <ul>
-          {allNames.names.map(name => <li key={name.name}>{name.name}</li>)}
+        {allNames.names.map(name => <li key={name.name}>{name.name}: {name.amount}</li>)}
         </ul>
 
-        {Object.keys(allNames).map(name => <li key={name}>{name.amount}</li>)}
-*/
+ */
+
 export default App;
